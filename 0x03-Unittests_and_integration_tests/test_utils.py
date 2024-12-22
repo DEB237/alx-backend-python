bdbbb62807a28d38df32e71dd83b2ffa4ed12d10
DEB237 <you@example.com>
2024-12-22 16:29:47 +0100

    Add decorator

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -20,17 +20,6 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-    @parameterized.expand([
-        ({}, ("a",)),
-        ({"a": 1}, ("a", "b")),
-    ])
-    def test_access_nested_map_exception(self, nested_map, path):
-        """
-        Test that access_nested_map raises a KeyError for invalid paths.
-        """
-        with self.assertRaises(KeyError) as context:
-            access_nested_map(nested_map, path)
-        self.assertEqual(str(context.exception), str(path))
-
 if __name__ == "__main__":
     unittest.main()
+
