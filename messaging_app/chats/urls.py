67ee425de84e57fd80b2f7ee783f9086355e0505
DEB237 <you@example.com>
2024-12-29 20:59:10 +0100

    Update urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -2,10 +2,16 @@ from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 from .views import ConversationViewSet, MessageViewSet
 
+# Create a DefaultRouter instance
 router = DefaultRouter()
+
+# Register the ConversationViewSet
 router.register(r'conversations', ConversationViewSet, basename='conversation')
+
+# Register the MessageViewSet
 router.register(r'messages', MessageViewSet, basename='message')
 
+# Define the urlpatterns
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(router.urls)),  # Include the DefaultRouter-generated URLs
 ]
